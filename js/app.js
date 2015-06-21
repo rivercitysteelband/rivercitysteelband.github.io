67a40a86e8c03c443d304ca34d4a370271dd5b1b
renewooller <renwooller@gmail.com>
2015-06-22 07:30:02 +1000

    flex slider working

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(window).load(function() {
       $('.flexslider').flexslider({
             animation: "fade",
           controlNav: false,
-          directionNav: true,
+          directionNav: false,
           slideshowSpeed: 5000,
           animationSpeed: 600,
           touch: true
